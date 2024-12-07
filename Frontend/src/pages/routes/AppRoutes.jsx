939fb90c3e7446032468b57b98f88600ff7ac449
Anant2929 <guptaanant53@gmail.com>
2024-12-07 17:31:23 +0530

    Changes in AppRoutes

--- Frontend/src/pages/routes/AppRoutes.jsx
+++ Frontend/src/pages/routes/AppRoutes.jsx
@@ -6,7 +6,7 @@ import Home from "../Home/Home.jsx";
 import ParcelRegistration from "../Parcels/ParcelRegistration.jsx"
 import ParcelTracking from '../Tracking/Tracking.jsx';
 import CarriersList from '../Parcels/CarrierList.jsx';
-import Confirmation from '../../Notification/ReciverConfirmationMessage.jsx';
+import ParcelAcceptanceForm from '../../Notification/ReciverConfirmationMessage.jsx';
 import { useAuth } from '../../context/AuthContext'; // Import the Auth context
 
 import ParcelNotification from '../../Notification/ReciverConfirmationMessage.jsx';
@@ -35,7 +35,7 @@ export default function AppRoutes() {
         <Route path="/oAuth/auth/google" element={token ? <Navigate to="/home" /> : <Layout />} />
         <Route path="/parcel/details" element={ token ? <ParcelRegistration/> : <Navigate to="/home" />} />
         <Route path="/trackingStatus" element={token ? <ParcelTracking /> : <Layout /> } />
-        <Route path="/home/receiverConfirm" element={ token ? <Confirmation /> : <Layout /> } />
+        <Route path="/home/receiverConfirm" element={ token ? <ParcelAcceptanceForm /> : <Layout /> } />
         <Route path="/home/parcel" element={token ? <ParcelNotification /> : <Navigate to="/home" /> } />
         <Route path="/home/parcel/CarriersList" element={token ? <CarriersList /> : <Navigate to="/home" /> } />
              {/* Renders the parcel notification modal */}
