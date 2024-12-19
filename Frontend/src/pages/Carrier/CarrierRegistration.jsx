e218f635bb83ec38f5ed51d346f764d269a5f59f
Suhanigupta2003 <suhaniharsh123@gmail.com>
2024-12-19 18:46:31 +0530

    carrier info confirm page addded

--- Frontend/src/pages/Carrier/CarrierRegistration.jsx
+++ Frontend/src/pages/Carrier/CarrierRegistration.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {useCarrierRegistration} from "../../context/CarrierContext";
 import CarrierDetails from './CarrierDetails';
 import TravelDetails from './TravelDetails';
-
+import CarreirInfoConfirmation from './CarrierInfoConfirmation'
 
 function CarrierRegistration() {
 
@@ -11,6 +11,7 @@ function CarrierRegistration() {
     <div>
       {CarriercurrentState === 1 && <CarrierDetails/>}
       {CarriercurrentState === 2 && <TravelDetails/>}
+      {CarriercurrentState === 3 && <CarreirInfoConfirmation/>}
     </div>
   )
 }
