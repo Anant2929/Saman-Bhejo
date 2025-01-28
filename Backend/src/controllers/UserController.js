655fffb885e1a5989b9d585c44194b8063b5c09d
Suhanigupta2003 <suhaniharsh123@gmail.com>
2025-01-28 12:38:10 +0530

    login deploment correction

--- Backend/src/controllers/UserController.js
+++ Backend/src/controllers/UserController.js
@@ -75,7 +75,7 @@ const login = async (req, res) => {
     // Send the token as a cookie
     res.cookie("token", token, {
       httpOnly: true, // Cookie is accessible only by the web server, helps with security
-      secure: true, // Use secure cookies in production
+      secure: process.env.NODE_ENV === "production", // Use secure cookies in production
       sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000 * 1,
     });
